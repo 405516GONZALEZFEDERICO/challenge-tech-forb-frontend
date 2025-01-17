04f411675abea95847b6d92f227dbf6b814f1829
405516_Gonzalez_Federico <405516@tecnicatura.frc.utn.edu.ar>
2025-01-16 23:06:52 -0300

    Create environment.prod.ts

---
+++ src/app/enviroments/environment.prod.ts
@@ -0,0 +1,13 @@
+const BASE_URL = 'http://localhost:8081/';
+const API_METRICS="https://my-json-server.typicode.com/405516GONZALEZFEDERICO/fake-api-metrics/environmentalData";
+export const environment = {
+  production: false,
+  services: {
+    login: `${BASE_URL}auth/login`,
+    register: `${BASE_URL}auth/register`,
+    refresh:`${BASE_URL}auth/refresh`,
+    getPlants:`${BASE_URL}plant/getPlants2`,
+    getMetrics:`${API_METRICS}`,
+    postPlant:`${BASE_URL}plant/newPlant`,
+  }
+};
